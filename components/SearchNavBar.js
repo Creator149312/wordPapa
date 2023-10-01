1ef288bab9e6379dd0adfb9a79b0c2cd66186dc8
Creator149312 <gauravsingh9314@gmail.com>
2023-10-01 16:32:34 +0530

    SearchNav select updated with memory

--- components/SearchNavBar.js
+++ components/SearchNavBar.js
@@ -1,48 +1,52 @@
-'use client'
-import {useState} from 'react';
+"use client";
+import { useState } from "react";
 const SearchBarNav = () => {
-  const [selectedUrl, setSelectedUrl] = useState('');
-  const [word, setWord] = useState('');
+  const [selectedOption, setSelectedOption] = useState("/adjectives-finder/");
+  const [word, setWord] = useState("");
+
   const urlOptions = [
-    { value: '/adjectives-finder/', label: 'Find Adjectives' },
-    { value: '/rhyming-words/', label: 'Rhyming Words' },
-    { value: '/similar-words/', label: 'Similar Words' },
-    { value: '/homophones-finder/', label: 'Homophones Words' },
+    { value: "/adjectives-finder/", label: "Find Adjectives" },
+    { value: "/rhyming-words/", label: "Rhyming Words" },
+    { value: "/similar-words/", label: "Similar Words" },
+    { value: "/homophones-finder/", label: "Homophones Words" },
   ];
 
-  const handleUrlChange = (e) => {
-    setSelectedUrl(e.target.value);
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
   };
 
   const handleLoadUrl = () => {
-    if (selectedUrl) {
-      window.location.href = selectedUrl;
+    if (selectedOption && word) {
+      window.location.href = selectedOption + word + "/";
     }
   };
 
   return (
     <>
-    <div className="search-bar-nav">
-      <select
-        value={selectedUrl}
-        onChange={handleUrlChange}
-        className="search-select input-lg"
-      ><option>Choose Your Operation </option>
-        {urlOptions.map((option) => (
-          <option key={option.value} value={ option.value +""+word}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      <input
-      className="input-lg search-input"
-        type="text"
-        placeholder="Type Your Word or Phrase"
-        onChange={(e) => setWord(e.target.value)}
-      />
-      <button onClick={handleLoadUrl} className="search-button">Search</button>
-    </div>
-  </>
+      <div className="search-bar-nav">
+        <select
+          value={selectedOption}
+          onChange={handleOptionChange}
+          className="search-select input-lg"
+        >
+          {" "}
+          {urlOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <input
+          className="input-lg search-input"
+          type="text"
+          placeholder="Type Your Word or Phrase"
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button onClick={handleLoadUrl} className="search-button">
+          Search
+        </button>
+      </div>
+    </>
   );
 };
 
