de29fc3a07360a36821f00b112f7bc6f2de80726
Creator1493 <gauravsingh9314@gmail.com>
2023-12-19 13:59:53 +0000

    GAnalytis code added

--- app/GAnalytics.js
+++ app/GAnalytics.js
@@ -1,13 +1,17 @@
+import Script from 'next/script'
+
 export default function GAnalytics() {
   return (<>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X136WTSXEM"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments)}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-X136WTSXEM');
-    </script>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-X136WTSXEM" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-X136WTSXEM');
+        `}
+      </Script>
     </>
   );
 }
