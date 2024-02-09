74effa5c354e23e3a221b85b2e5e3ee26e1a407b
Creator1493 <gauravsingh9314@gmail.com>
2024-02-09 19:15:19 +0000

    middleware Origin domains updated

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   console.log("Middleware is running");  
-  let OriginArrays = ["http://localhost:3000", "https://words.englishbix.com"];
+  let OriginArrays = ["http://localhost:3000", "https://words.englishbix.com", "https://localhost:3000"];
 
   if (!OriginArrays.includes(request.nextUrl.origin)) {
     return NextResponse.redirect(
