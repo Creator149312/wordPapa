b1967de3d8ebc36b588b70ab5e25d6fe388586d5
Creator149312 <gauravsingh9314@gmail.com>
2024-02-09 10:39:06 +0530

    fixed sitemap generation

--- app/browse/sitemap.js
+++ app/browse/sitemap.js
@@ -4,29 +4,26 @@ const BASE_URL = "https://words.englishbix.com";
 /**
  * This file is used to generate multiple sitemaps
  */
-function printAsciiCharacters(str) {
-  for (let i = 0; i < str.length; i++) {
-    const char = str.charAt(i);
-    const ascii = char.charCodeAt(0);
-    console.log(`Character: ${char}, ASCII: ${ascii}`);
-  }
-}
 
 async function getWords(l) {
   const filePath = process.cwd() + "/app/browse/actualWords.txt"; // Replace with the actual path to your file.
   const regex = /^[a-zA-Z]+$/;
+  const pluralSuffixes = ["s", "es", "ies", "ves"]; // Plural suffixes
+
   try {
     const fileContent = await fs.readFile(filePath, "utf8");
     const linksArray = fileContent.split("\n");
-    // console.log("Total Words = " + linksArray.length);
     if (l === "0") {
       return linksArray.filter(
         (word) => !/[a-zA-Z]/.test(word.charAt(0)) === true
       );
     } else {
+      // we want only those words which have length > 1 and are not plurals or with spaces or other special characters
       return linksArray.filter((word) => {
         word = word.trim();
-        if (word.charAt(0) === l && regex.test(word)) return true;
+        if (word.charAt(0) === l && regex.test(word) && word.length > 1) {
+          return !pluralSuffixes.some((suffix) => word.endsWith(suffix));
+        }
       });
     }
   } catch (error) {
@@ -36,13 +33,40 @@ async function getWords(l) {
 
 export async function generateSitemaps() {
   // calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+  return [
+    { id: "a-words" },
+    { id: "b-words" },
+    { id: "c-words" },
+    { id: "d-words" },
+    { id: "e-words" },
+    { id: "f-words" },
+    { id: "g-words" },
+    { id: "h-words" },
+    { id: "i-words" },
+    { id: "j-words" },
+    { id: "k-words" },
+    { id: "l-words" },
+    { id: "m-words" },
+    { id: "n-words" },
+    { id: "o-words" },
+    { id: "p-words" },
+    { id: "q-words" },
+    { id: "r-words" },
+    { id: "s-words" },
+    { id: "t-words" },
+    { id: "u-words" },
+    { id: "v-words" },
+    { id: "w-words" },
+    { id: "x-words" },
+    { id: "y-words" },
+    { id: "z-words" },
+  ];
 }
 
 export default async function sitemap({ id }) {
   // Google's limit is 50,000 URLs per sitemap
-  const products = await getWords(String.fromCharCode(97 + id));
-  console.log(products.length); 
+  const products = await getWords(id.split("-")[0]);
+
   return products.map((product) => ({
     url: `${BASE_URL}/define/${product}`.trim(),
     lastModified: new Date(),
