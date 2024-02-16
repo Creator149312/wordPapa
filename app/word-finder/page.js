9b14ac1b06a790d7440a1acf36dc4347a534799d
Creator149312 <gauravsingh9314@gmail.com>
2024-02-16 10:47:47 +0530

    updated word-finder

--- app/word-finder/page.js
+++ app/word-finder/page.js
@@ -1,42 +1,71 @@
 const ptitle = "Word Finder: Unscramble Letters to Make Words";
 
 export const metadata = {
-    title: ptitle,
-    description:
-        "Word Finder: Your Ultimate Word Unscrambler! Quickly find words from jumbled letters, enhance vocabulary, and ace word games. Perfect for students, teachers, and word enthusiasts. Filter words and Unlock the secrets of scrambled words and have fun with language!",
+  title: ptitle,
+  description:
+    "Word Finder: Your Ultimate Word Unscrambler! Quickly find words from jumbled letters, enhance vocabulary, and ace word games. Perfect for students, teachers, and word enthusiasts. Filter words and Unlock the secrets of scrambled words!",
 };
 
 function WordFinderPage() {
-    return (
+  return (
+    <div>
+      <div className="row">
         <div>
-            <div className="row">
-                <div>
-                    {/* <SimilarWords /> */}
-                    {/* Continue adding content */}
-                    <div className="m-3 p-3">
-                        <h1>{ptitle}</h1>
-                        <p>
-                            Word Finder is a powerful tool that helps unscramble jumbled letters and find all the words you can form with given letters.
-                            It's super handy because it quickly sorts out the mixed-up letters, saving you time and effort.</p>
-                        <p>Ever found yourself scratching your head over a mess of jumbled letters, desperately trying to untangle them into coherent words? If so, let me introduce you to your new best friend: Word Finder! This nifty tool is like having a super-smart sidekick who can unscramble words faster than you can say "alphabet soup," saving you heaps of time and frustration.</p>
-
-                        <p>But wait, there's more! Word Finder isn't your run-of-the-mill word unscrambler—it's packed with features that make it a powerhouse for word enthusiasts of all stripes.
-                            With options to filter words based on length, pattern, or even known letters, it's like having a Swiss army knife for wordplay at your fingertips.
-                            Whether you're a word nerd looking to expand your vocabulary or a student trying to ace your next spelling test, Word Finder has got you covered.
-                        </p><p>
-                            And let's not forget about the fun factor! When you're not busy impressing your friends with your word wizardry, It doubles as your go-to cheat for word games and puzzles.
-                            From helping in Scrabble showdowns to cracking crossword clues, it's the ultimate tool for turning downtime into playtime.
-                        </p><p>
-                            But here's the real kicker: Word Unscrambler isn't just for kids or casual word enthusiasts—it's for everyone! Teachers can use it to spice up their lesson plans with interactive word games, while professionals can rely on it for crafting polished resumes or writing snappy emails.
-                            Whether you're a student, a teacher, a professional, or just someone who loves to play with words, It is your trusty sidekick for unraveling the mysteries of scrambled letters and having a blast with language.
-                        </p><p>
-                            So why wait? Give Word Finder a spin today and prepare to be amazed at how it transforms your wordplay game from "meh" to "marvelous" in no time.
-                            With Word Finder by your side, the only limit to your linguistic adventures is your imagination!</p>
-                    </div>
-                </div>
-            </div>
+          <div className="m-3 p-3">
+            <h1>{ptitle}</h1>
+            <p>
+              Word Finder is a powerful tool that helps unscramble jumbled
+              letters and find all the words you can form with given letters.
+              It's super handy because it quickly sorts out the mixed-up
+              letters, saving you time and effort.
+            </p>
+            <p>
+              Ever found yourself scratching your head over a mess of jumbled
+              letters, desperately trying to solve them into coherent words?
+              If so, let me introduce you to your new best friend: Word Finder!
+              This free tool is like having a super-smart sidekick who can
+              unscramble words faster than you can say "alphabet soup," saving
+              you heaps of time and frustration.
+            </p>
+            <p>
+              But wait, there's more! Word Finder isn't your run-of-the-mill
+              word unscrambler—it's packed with advanced features that make it a
+              powerhouse for word enthusiasts of all stripes. With options to
+              filter words based on length, pattern, or even known letters, it's
+              like having a Swiss army knife for wordplay at your fingertips.
+              Whether you're a word nerd looking to expand your vocabulary or a
+              student trying to ace your next spelling test, Word Finder has got
+              you covered.
+            </p>
+            <p>
+              And let's not forget about the fun factor! When you're not busy
+              impressing your friends with your word wizardry, It doubles as
+              your go-to cheat for word games and puzzles. From helping in
+              Scrabble showdowns to cracking crossword clues, it's the ultimate
+              tool for turning downtime into playtime. It works online, so you
+              can use it on your phone or computer from anywhere.
+            </p>
+            <p>
+              But here's the real kicker: Word Unscrambler isn't just for kids
+              or casual word enthusiasts—it's for everyone! Teachers can use it
+              to spice up their lesson plans with interactive word games, while
+              professionals can rely on it for crafting polished resumes or
+              writing snappy emails. Whether you're a student, a teacher, a
+              professional, or just someone who loves to play with words, It is
+              your trusty sidekick for solving the mysteries of scrambled
+              letters and having a blast with language.
+            </p>
+            <p>
+              So why wait? Give Word Finder a spin today and prepare to be
+              amazed at how it transforms your wordplay game from "meh" to
+              "marvelous" in no time. With Word Finder by your side, the only
+              limit to your linguistic adventures is your imagination!
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default WordFinderPage;
