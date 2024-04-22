f4fdb4bb8e2b90b4da24174820ccaaa8a10db001
Creator149312 <gauravsingh9314@gmail.com>
2024-04-23 02:43:38 +0530

    1st Working version of password reset by Token

--- app/new-password/page.js
+++ app/new-password/page.js
@@ -1,11 +1,12 @@
 // import ResetPasswordForm from "@components/ResetPasswordForm";
+import { Suspense } from "react";
 import ResetPasswordbyToken from "../../components/ResetPasswordbyToken";
 
 export default async function SettingsPage() {
   return (
-    <>
-    <h1>Reset Password</h1>
+    <Suspense>
+      <h1>Reset Password</h1>
       <ResetPasswordbyToken />
-    </>
+    </Suspense>
   );
 }
