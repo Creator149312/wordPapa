d7a0408d93b4a5af8602248bb9557b158409aa0f
Creator149312 <gauravsingh9314@gmail.com>
2024-05-05 10:03:12 +0530

    testing ads

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,39 +1,40 @@
-'use client'
-import GoogleAd from '@utils/GoogleAd';
-
-import {  CardContent, CardHeader } from "@components/ui/card";
+"use client";
+// import GoogleAd from "@utils/GoogleAd";
+import { CardContent, CardHeader } from "@components/ui/card";
+import AdsUnit from "./AdsUnit";
 
 const SideBar = () => {
-  return (<>
-          <div className=''>
-            <CardHeader>
+  return (
+    <>
+      <div className="">
+        <CardHeader>
           <h2 className="text-3xl font-extrabold">Related Workbooks</h2>
-          </CardHeader>
-          <CardContent>
-            <ul>
-              <li className='no-style-list'>
-                <a href="https://www.englishbix.com/product/35-word-family-words-tracing-workbook/">
-                  Rhyming Words Tracing Workbook
-                </a>
-              </li>
-              <li className='no-style-list'>
-                <a href="https://www.englishbix.com/product/3-letter-rhyming-words-workbook/">
-                  3 Letter Rhyming Words Workbook
-                </a>
-              </li>
-            </ul>
-          </CardContent>
-            {/* <div>
-              <GoogleAd slotID={3722270586}/>
-            </div>  */}
-          </div>
-          <div className="m-3">
-            {/* <div>
-                <GoogleAd slotID={3722270586}/>
-            </div> */}
-          </div>
-          </>
-  )
-}
+        </CardHeader>
+        <CardContent>
+          <ul>
+            <li className="no-style-list">
+              <a href="https://www.englishbix.com/product/35-word-family-words-tracing-workbook/">
+                Rhyming Words Tracing Workbook
+              </a>
+            </li>
+            <li className="no-style-list">
+              <a href="https://www.englishbix.com/product/3-letter-rhyming-words-workbook/">
+                3 Letter Rhyming Words Workbook
+              </a>
+            </li>
+          </ul>
+        </CardContent>
+        <div>
+          <AdsUnit slotID={3722270586} />
+        </div>
+      </div>
+      <div className="m-3">
+        <div>
+          <AdsUnit slotID={3722270586} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SideBar
+export default SideBar;
