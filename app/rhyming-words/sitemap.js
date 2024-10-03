f4f35b8be5a79fd2f0a4320d81f37b7cc803dc93
Creator149312 <gauravsingh9314@gmail.com>
2024-10-03 15:59:58 +0530

    sitemaps updated for /rhyming-words

--- app/rhyming-words/sitemap.js
+++ app/rhyming-words/sitemap.js
@@ -1,10 +1,25 @@
 import rhymingWordsArray from "./rhyming-words";
 const BASE_URL = "https://words.englishbix.com";
 
-export const revalidate = 36000;
+export const revalidate = 360;
 
 export default async function sitemap({ id }) {
-  return rhymingWordsArray.map((word) => ({
+
+  const AllWords = new Set();
+
+  // const regex = /^\S+$/; // Matches any word that doesn't contain spaces
+  const regex = /^[a-zA-Z0-9 -]+$/; //to find words which contain characters or digits 
+  const simpleRegex = /^[a-zA-Z0-9]+$/; // to find words which only contain letters
+
+  rhymingWordsArray.forEach((word) => {
+    if (simpleRegex.test(word)) {
+      AllWords.add(word);
+    }
+  });
+
+  const AllWordsArray = [...AllWords];
+
+  return AllWordsArray.map((word) => ({
     url: `${BASE_URL}/rhyming-words/${word}`.trim(),
     lastModified: new Date(),
   }));
